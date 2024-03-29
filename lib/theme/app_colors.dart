b73374fa360935520d3a8e97fe88499aa41884a3
Bon Tertius Tuyishimire <tuyishimirebt12@gmail.com>
2024-03-29 17:20:21 +0200

    done setting up - ready to start coding 😉

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const green = Color(0xFF106F45);
+  static const red = Color(0xFFAF332B);
+  static const darkGray = Color(0xFF373737);
+  static const lightGray = Color(0xFFADADAD);
+  static const black = Color(0xFF1A1A19);
+  static const white = Color(0xFFE7E7E7);
+  static const yellow = Color(0xFFFFB027);
+}
